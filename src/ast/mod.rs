7f2fb81267fda5420649a88f92f9f5cdd6d6f2ff
atmatm9182 <atmatm9182@gmail.com>
2024-03-05 19:16:55 +0100

    WHOOPS! Starting to rewrite everything in zig!

--- src/ast/mod.rs
+++
@@ -1,9 +0,0 @@
-mod expr;
-mod statement;
-mod program;
-
-pub use program::Program;
-pub use statement::*;
-pub use expr::*;
-
-pub type Ident<'a> = &'a str;
--- src/ast/program.rs
+++
@@ -1,4 +0,0 @@
-use super::statement::Statement;
-
-#[derive(Debug)]
-pub struct Program<'s>(pub Vec<Statement<'s>>);
